8c215e47dbcc41cf1c974eaaeb1567d87563b256
Sudhir Rao <sudrao@gmail.com>
2011-09-01 09:58:39 -0700

    Prep for evernote integration.

---
+++ scripts/get_note.rb
@@ -0,0 +1,20 @@
+require 'evernote'
+require 'fileutils'
+require 'yaml'
+
+class GetNote
+
+  def self.run
+    user_store_url = "https://sandbox.evernote.com/edam/user"
+    config = YAML::load_file('evernote.yml')
+    user_store = Evernote::UserStore.new(user_store_url, config)
+
+    auth_result = user_store.authenticate
+    user = auth_result.user
+    auth_token = auth_result.authenticationToken
+    puts "Authentication was successful for #{user.username}"
+    puts "Authentication token = #{auth_token}"
+  end
+end
+
+GetNote.run
