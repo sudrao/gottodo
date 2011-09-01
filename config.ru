2cc0b7031e308c206dd870f56c0ef62f43cdff9b
Sudhir Rao <sudrao@gmail.com>
2011-09-01 10:32:33 -0700

    Add Rails 3.1 blank prior to conversion.

--- config.ru
+++
@@ -1,4 +0,0 @@
-require 'gottodo'
-
-run Sinatra::Application
-
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gottodo::Application.config.secret_token = 'abec399ee959bfe50acb05cec42f96fbfa2ebaf22976f465e9ea63a6f38c955ce4a125da9d636554a6f4392478ab7d8b0ea6b704cbaea9e14c72491c154c8657'
